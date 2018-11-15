b3a6f382095c63fd5a8164f3e6b4ee95c1ae3230
Aren Leishman <aren.leishman@gmail.com>
2018-11-15 19:35:09 +0800

    Fix inaccurate counting of messages

--- scraper.py
+++ scraper.py
@@ -23,42 +23,45 @@ class Scraper:
             else:
                 table_last -= 1
 
-    async def update(self, c):
+    async def update(self, channel):
         # Open the table
         bar = tqdm.tqdm(total=0, unit=' messages', file=open(os.devnull, 'w'))
         message_return = await self.client.send_message(self.response_channel, bar)
-        last_edit = time.time()
-        table = self.database.table(c.id)
+        last_edit = time.time()  # last edit of the progress bar
+        table = self.database.table(channel.id)
         # If the channel has never been scraped before...
         if len(table) == 0:
             last_scraped_msg = None
         # If the channel has been scraped before, go through the list of last messages...
         else:
-            last_scraped_msg = await self.determine_start_message(table, c)
-        protected_table = self.database.table('protected')
-        if protected_table.get(Query().id == c.id):
-            return 0  # Just end here.
+            last_scraped_msg = await self.determine_start_message(table, channel)
+        protected_table = self.database.table('protected') # Channels that shouldn't be scraped
+        if protected_table.get(Query().id == channel.id):
+            return 0  # Just end here, we aren't scraping this channel.
         # IDE may complain but that's because it doesn't know self.client is a discord.Client()
         table_cache = []
-        async for fetched_message in self.client.logs_from(c, after=last_scraped_msg, limit=10000000000):
+        async for fetched_message in self.client.logs_from(channel, after=last_scraped_msg, limit=10000000000):
             time_now = time.time()
             table_cache.append({'id': fetched_message.id, 'timestamp': fetched_message.timestamp.timestamp(),
                                 'author': fetched_message.author.id, 'author_name': str(fetched_message.author),
-                                'content': fetched_message.content})
+                                'content': fetched_message.content})  # Actually put the message in the DB
             bar.update(1)
-            if time_now - last_edit >= 2:
+            if time_now - last_edit >= 2:  # compare time since progress bar was last updated (default 2 seconds)
                 updated = str(bar)
                 await self.client.edit_message(message_return, updated)
                 last_edit = time.time()
         table_cache.reverse()
         table.insert_multiple(table_cache)
+        # Update the bar one last time
+        updated = str(bar)
+        await self.client.edit_message(message_return, updated)
         bar.close()
         del bar
 
     async def update_all(self):
-        for c in self.server.channels:
-            await self.client.send_message(self.response_channel, "Now fetching #" + c.name)
-            await self.update(c)
+        for channel in self.server.channels:
+            await self.client.send_message(self.response_channel, "Now fetching #" + channel.name)
+            await self.update(channel)
 
     async def update_one(self, channel_to_update):
         await self.client.send_message(self.response_channel, "Now fetching #" + channel_to_update.name)
